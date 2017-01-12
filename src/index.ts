afa7ad905600809eb5433fa694bdcedc9fd6fe54
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-12 14:07:44 +0300

    Add index file with public interface and params validations

---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import * as has from 'lodash/has';
+
+import * as storage from './modules/storage';
+
+import {
+  Config,
+  Client,
+  User,
+  EventName,
+  EventRequest,
+  ClickSuggestionEventRequest,
+  ClickItemEventRequest,
+  RedirectEventRequest,
+  PurchaseEventRequest,
+  UpdateCartEventRequest,
+  AddToCartEventRequest,
+  ViewPageEventRequest,
+} from './types';
+
+function init(config: Config): Client {
+  if (!has(config, 'key')) {
+    throw new Error('"key" param is required');
+  }
+
+  // 30 min lifetime - sid
+  // unlimited - uid
+
+  return {
+    getUser(): User {
+      return {
+        uid: '',
+        sid: '',
+      };
+    },
+    sendEvent(name: EventName, request: EventRequest) {
+      if (name === 'click-suggestion') {
+        if (!has(request, 'rid')) {
+          throw new Error('"rid" param is required');
+        }
+
+        if (!has(request, 'suggestion')) {
+          throw new Error('"suggestion" param is required');
+        }
+      }
+
+      if (name === 'click-item' && !has(request, 'item_id')) {
+        throw new Error('"item_id" param is required');
+      }
+
+      if (name === 'redirect') {
+        if (!has(request, 'rid')) {
+          throw new Error('"rid" param is required');
+        }
+
+        if (!has(request, 'suggestion')) {
+          throw new Error('"suggestion" param is required');
+        }
+      }
+
+      if (name === 'purchase') {
+        if (!has(request, 'order_id')) {
+          throw new Error('"order_id" param is required');
+        }
+
+        if (!has(request, 'currency')) {
+          throw new Error('"currency" param is required');
+        }
+
+        if (!has(request, 'revenue')) {
+          throw new Error('"revenue" param is required');
+        }
+
+        if (!has(request, 'line_items')) {
+          throw new Error('"line_items" param is required');
+        }
+
+        if (!(request as PurchaseEventRequest).line_items.every((item) => !! item.item_id)) {
+          throw new Error('"line_items[].item_id" param is required');
+        }
+
+        if (!(request as PurchaseEventRequest).line_items.every((item) => !! item.unit_price)) {
+          throw new Error('"line_items[].unit_price" param is required');
+        }
+
+        if (!(request as PurchaseEventRequest).line_items.every((item) => !! item.quantity)) {
+          throw new Error('"line_items[].quantity" param is required');
+        }
+      }
+
+      if (name === 'add-to-cart' && !has(request, 'item_id')) {
+        throw new Error('"item_id" param is required');
+      }
+
+      if (name === 'update-cart') {
+        if (!has(request, 'line_items')) {
+          throw new Error('"line_items" param is required');
+        }
+
+        if (!(request as UpdateCartEventRequest).line_items.every((item) => !! item.item_id)) {
+          throw new Error('"line_items[].item_id" param is required');
+        }
+
+        if (!(request as UpdateCartEventRequest).line_items.every((item) => !! item.unit_price)) {
+          throw new Error('"line_items[].unit_price" param is required');
+        }
+
+        if (!(request as UpdateCartEventRequest).line_items.every((item) => !! item.quantity)) {
+          throw new Error('"line_items[].quantity" param is required');
+        }
+      }
+
+      // set by default `url`, `width`, `height`, `ref` props in `view-page` event
+    },
+  };
+}
+
+export {
+  init,
+}
