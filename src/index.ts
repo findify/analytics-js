69559ed28cefcf45dd7819f265088ecc7b3cbc48
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-13 12:37:39 +0300

    Implement sendEvent function body

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as has from 'lodash/has';
 import * as storage from './modules/storage';
 
+import { requestApi } from './modules/requestApi';
 import { uuid } from './utils/uuid';
 import { validateSendEventParams, validateInitParams } from './validations';
 
@@ -35,28 +36,90 @@ function init(config: Config): Client {
     sendEvent(name: EventName, request: EventRequest) {
       validateSendEventParams(name, request);
 
+      const user = this.getUser();
+
       if (name === 'click-suggestion') {
+        requestApi({
+          user,
+          event: 'click-suggestion',
+          properties: {
+            rid: request.rid,
+            suggestion: request.suggestion,
+          },
+        });
       }
 
       if (name === 'click-item') {
+        requestApi({
+          user,
+          event: 'click-item',
+          properties: {
+            rid: request.rid,
+            item_id: request.item_id,
+          },
+        });
       }
 
       if (name === 'redirect') {
+        requestApi({
+          user,
+          event: 'redirect',
+          properties: {
+            rid: request.rid,
+            suggestion: request.suggestion,
+          },
+        });
       }
 
       if (name === 'purchase') {
+        requestApi({
+          user,
+          event: 'purchase',
+          properties: {
+            order_id: request.order_id,
+            currency: request.currency,
+            revenue: request.revenue,
+            line_items: request.line_items,
+            affiliation: request.affiliation,
+          },
+        });
       }
 
       if (name === 'add-to-cart') {
+        requestApi({
+          user,
+          event: 'add-to-cart',
+          properties: {
+            rid: request.rid,
+            item_id: request.item_id,
+            quantity: request.quantity,
+          },
+        });
       }
 
       if (name === 'update-cart') {
+        requestApi({
+          user,
+          event: 'purchase',
+          properties: {
+            line_items: request.line_items,
+          },
+        });
       }
 
       if (name === 'view-page') {
+        requestApi({
+          user,
+          event: 'view-page',
+          properties: {
+            url: window.location.href,
+            ref: window.document.referrer,
+            width: window.screen.width,
+            height: window.screen.height,
+            item_id: request.item_id,
+          },
+        });
       }
-
-      // set by default `url`, `width`, `height`, `ref` props in `view-page` event
     },
   };
 }
