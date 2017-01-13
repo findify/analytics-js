1e8e7879bcb9ee61335c2cea73d073f4a04a5c76
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-13 20:21:21 +0300

    Move storage handling in jsdom helper

--- __tests__/jsdom-helper.ts
+++ __tests__/jsdom-helper.ts
@@ -8,19 +8,56 @@ declare const global: {
 function setupJsDom(onInit?) {
   jsdom.env({
     html: '<!DOCTYPE html><html><head></head><body></body></html>',
+    url: 'http://jsdom-url.com',
+    document: {
+      referrer: 'http://jsdom-referrer-url.com',
+    },
     features: {
       FetchExternalResources: ['script', 'img'],
       ProcessExternalResources: ['script'],
     },
     done: (err, window) => {
       global.window = window;
-      global.document = window.document;
+      global.document = global.window.document;
+
+      initCookies(global.window);
+      initLocalStorage(global.window);
 
       if (onInit) {
         onInit();
       }
     },
   });
+}
+
+function initCookies(w) {
+  let cookie = '';
+
+  Object.defineProperty(w.document, 'cookie', {
+    configurable: true,
+    get() {
+      return cookie;
+    },
+    set(value) {
+      cookie += !cookie ? value : '; ' + value;
+    },
+  });
+}
+
+function initLocalStorage(w) {
+  let storage = {};
+
+  w.localStorage = {
+    setItem(key, value) {
+      storage[key] = value;
+    },
+    getItem(key) {
+      return storage[key] || null;
+    },
+    removeItem(key) {
+      delete storage[key];
+    },
+  };
 }
 
 function teardownJsDom() {
